01b9093a5e64ac9f2ea36747d7f952f1a9424e69
s-Woq <midwardisaac@gmail.com>
2023-11-29 16:53:20 -0500

    addind changes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from py_files.stabledif import CelebAPreprocessing 
+from py_files.iter_img import processImages
 
 
 def run():
     preprocessor=CelebAPreprocessing()
+    processImages("./")
     
 
 
